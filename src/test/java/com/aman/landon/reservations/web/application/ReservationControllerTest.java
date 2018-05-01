a225784e9d45727e4872699615bd2edd03de31f4
amanbahata <bahataaman@gmail.com>
2018-05-01 15:57:30 +0100

    Adds ReservationsController

---
+++ src/test/java/com/aman/landon/reservations/web/application/ReservationControllerTest.java
@@ -0,0 +1,22 @@
+package com.aman.landon.reservations.web.application;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReservationControllerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getReservations() throws Exception {
+    }
+
+}
